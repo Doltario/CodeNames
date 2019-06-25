2f0d9ff58adb7dde09b185251909842f938f2ef8
Félix Darraud <felix.darraud@gmail.com>
2019-06-25 14:36:22 +0200

    Init

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const redNumber = 7;
+    const blueNumber = 7;
+    const blackNumber = 1;
+    const wordsList = [
+        'zrg',
+        'ergojk',
+    ];
+    const colorsMap = { black : 'black', red: 'red', blue: 'blue'};
+
+
+    const grid = []; // { value: Word, color: Blue, Red, Black}
+
+    fillWords = () => {
+        //call api
+        // fill wordsList
+    }
+
+
+    writeCase = () => {
+
+    } 
+
+    const wordsContainer = document.querySelector('word-list');
+
+    for (let i = 0; i < redNumber; i++) {
+        
+    }
+});
