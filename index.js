ce14ee089ee01314982372fd4a537cdc30639602
Félix Darraud <felix.darraud@gmail.com>
2019-06-25 15:31:22 +0200

    WIP on case generation

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const redNumber = 7;
-    const blueNumber = 7;
-    const blackNumber = 1;
+    const cardNumber = 7;
+
+    const gridLimit = 25;
+
+    let redToWrite = cardNumber;
+    let blueToWrite = cardNumber;
+    let blackToWrite = 1;
+
     const wordsList = [
         'zrg',
         'ergojk',
     ];
-    const colorsMap = { black : 'black', red: 'red', blue: 'blue'};
 
+    const colorsMap = { black : 'black', red: 'red', blue: 'blue'};
 
     const grid = []; // { value: Word, color: Blue, Red, Black}
 
@@ -16,14 +21,36 @@ document.addEventListener('DOMContentLoaded', () => {
         // fill wordsList
     }
 
+    _writeCase = (word, color) => {
+        const caseItem = { color: color, word : word };
+        grid.push(caseItem);
+    }
 
     writeCase = () => {
+        const word = wordsList[Math.floor(Math.random() * wordsList.length)];
+        const writtenCase = null;
 
-    } 
+        var r = Math.random();
+        if (r < 0.20 && redToWrite > 0) {
+            _writeCase(word, colorsMap.red);
+            redToWrite--;
+        } else if (r < 0.4 && blueToWrite > 0) {
+            _writeCase(word, colorsMap.blue);
+            blueToWrite--;
+        } else if (r < 0.6 && blackToWrite > 0) {
+            _writeCase(word, null);
+            blackToWrite--;
+        } else if (r < 0.9) {
+            _writeCase(word, colorsMap.black);
+        }
+    }
 
     const wordsContainer = document.querySelector('word-list');
 
-    for (let i = 0; i < redNumber; i++) {
+    for (let i = 0; i < gridLimit; i++) {
+        console.log(i);
         
+        writeCase();
     }
+    console.log(grid);
 });
